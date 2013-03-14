b0735297de249d4db48b5eeda084aefecf62d749
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2013-03-14 14:57:07 +0100

    removed camptocamp specific needs

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,22 +18,8 @@ class logstash (
     ensure  => 'directory',
   }
 
-  case $::osfamily {
-    'RedHat': {
-      package {'logstash':
-        ensure => latest,
-      }
-    }
-    'Debian': {
-      apt::preferences {'logstash':
-        pin      => "version ${version}~c2c*",
-        priority => 1001,
-      }
-      package {'logstash':
-        ensure  => latest,
-        require => Apt::Preferences ['logstash'],
-      }
-    }
+  package {'logstash':
+    ensure => ensure,
   }
 
   user {$user:
