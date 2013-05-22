9db4bcc9a1064dd77e7d42f0d0535c674d60ddb6
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2013-05-22 10:57:14 +0200

    doc + parameter validation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,40 @@
-class logstash ( 
+# == Class: logstash
+#
+# Base class, which will install logstash. To declare logstash intances use
+# defined type `logstash::instance`
+#
+# === Parameters
+#
+# [*home*]
+#   Directory where logstash jar file is located.
+#
+# [*etc*]
+#   Directory where logstash configuration is located.
+#
+# [*user*]
+#   Default logstash user.
+#
+# [*group*]
+#   Default logstash group.
+#
+# === Examples
+#
+# See README
+#
+class logstash (
   $home    = '/usr/share/logstash',
   $etc     = '/etc/logstash',
   $log     = '/var/log/logstash',
   $user    = 'logstash',
   $group   = 'logstash',
 ) {
+
+  validate_string($logstash::home)
+  validate_absolute_path($logstash::home)
+  validate_string($logstash::etc)
+  validate_absolute_path($logstash::etc)
+  validate_string($logstash::log)
+  validate_absolute_path($logstash::log)
 
   class {'logstash::install': } ->
   class {'logstash::config': } ->
